29d36653073040bffc20984e75ed944fdd762d20
James Zhang <jameszhanga@gmail.com>
2022-03-05 01:05:39 -0600

    Implement google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as ga from '../lib/analytics/';
 
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(
+    () => {
+      const handleRouteChange = (url) => {
+        ga.pageview(url);
+      };
+      router.events.on('routeChangeComplete', handleRouteChange);
+
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    },
+    [ router.events ]
+  );
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
